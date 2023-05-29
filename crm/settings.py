5af3ac719bf692359c852b38e9fed3cf50768eb2
hikmatillo <dolgovdaniil007@gmail.com>
2023-05-29 21:44:34 +0700

    updated settings.py

--- crm/settings.py
+++ crm/settings.py
@@ -77,15 +77,15 @@ WSGI_APPLICATION = 'wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-if DEBUG:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
-else:
-    DATABASES = {
+# if DEBUG:
+#     DATABASES = {
+#         'default': {
+#             'ENGINE': 'django.db.backends.sqlite3',
+#             'NAME': BASE_DIR / 'db.sqlite3',
+#         }
+#     }
+# else:
+DATABASES = {
         'default': {
              'ENGINE': 'django.db.backends.postgresql',
              'NAME': os.environ.get("TABLE_NAME", ''),
